e30deb441818b61bf63a91b4f5f711920a607a68
dunxtand <mdrstandish@gmail.com>
2017-03-20 17:51:16 -0400

    add checks and warning to prototype extension

--- build/ssb.js
+++ build/ssb.js
@@ -296,5 +296,14 @@
     }
   })();
 
-  window.HTMLInputElement.prototype.shopifySearchBar = protoMethod;
+  try {
+    var constructor = window.HTMLInputElement || window.document.createElement("INPUT").constructor;
+    constructor.prototype.shopifySearchBar = protoMethod;
+  } catch (error) {
+    window.console.error(
+      "shopify-search-bar will not work in this browser!",
+      "Cannot access HTMLInputElement prototype:",
+      error
+    );
+  }
 })(window, document);
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var protoMethod = require("./src/protoMethod");
-window.HTMLInputElement.prototype.shopifySearchBar = protoMethod;
+try {
+  var constructor = window.HTMLInputElement || window.document.createElement("INPUT").constructor;
+  constructor.prototype.shopifySearchBar = protoMethod;
+} catch (error) {
+  window.console.error(
+    "shopify-search-bar will not work in this browser!",
+    "Cannot access HTMLInputElement prototype:",
+    error
+  );
+}
